f889a18cd24cdb1a6fbcfc44a8735a811c5df872
Faruq Afolabi <gbolayo13@gmail.com>
2024-07-10 16:37:01 -0400

    experiemnt update
    Updated the to include the rotating complex shapes for the follow up experiment

--- Assets/Scripts/ExpController.cs
+++ Assets/Scripts/ExpController.cs
@@ -146,10 +146,15 @@ public class ExpController : MonoBehaviour
     [SerializeField]
     protected float m_PlayerDistanceFromSetup = 0.8f;
     /// <summary>
-    /// Max reach distance of the player using the raycast
+    /// How fast the shape should rotate in DEG/S (Only set this if you're running ExpType 5)
     /// </summary>
-    /*[SerializeField]
-    protected float m_PlayerReachDistance = 10f;*/
+    [SerializeField]
+    protected float m_ShapeRotSpeed = 1f;
+    /// <summary>
+    /// Axis the shape should rotate on
+    /// </summary>
+    [SerializeField]
+    protected Vector3 m_ShapeRotAxis = new(0, 1, 0);
 
     /// <summary>
     /// Button for larger input response
@@ -171,12 +176,22 @@ public class ExpController : MonoBehaviour
         InteractiveSimple,
         NonInteractiveComplex,
         InteractiveComplex,
+        RotatingComplex
     }
 
     /// <summary>
     /// Experiment types
     /// </summary>
-    protected ExpType _expType;
+    private ExpType _expType;
+    
+    protected ExpType expType{
+        get { return _expType; }
+        set 
+        { 
+            _expType = value;
+            ExpTypeChanged();
+        }
+    }
     /// <summary>
     /// List of order of the experiment. Contains string in format "shapeNumber|shapeName|scale|spawnLocation";
     /// </summary>
@@ -203,12 +218,13 @@ public class ExpController : MonoBehaviour
     protected DateTime _entryTime = DateTime.Now;
 
     //static variables
-    protected readonly static string LARGER_RESPONSE = "1";
-    protected readonly static string SMALLER_RESPONSE = "0";
-    protected readonly static string NO_RESPONSE = "-1";
-    protected readonly static int DEFAULT_NO_SHAPE = 4;
-    protected readonly static int DEFAULT_NO_LARGER = 3;
-    protected readonly static int DEFAULT_NO_SMALLER = 3;
+    protected readonly static string LARGER_RESPONSE = "1";//save "1" for larger responses
+    protected readonly static string SMALLER_RESPONSE = "0";//save "0" for smaller responses 
+    protected readonly static string NO_RESPONSE = "-1";//save "-1" if n response is recorded
+    protected readonly static int DEFAULT_NO_SHAPE = 4;//4 shapes by default
+    protected readonly static int DEFAULT_NO_LARGER = 3;//3 sizes bigger for each shape
+    protected readonly static int DEFAULT_NO_SMALLER = 3;//3 sizes smaler for each shape
+    protected readonly static float MAX_SHAPE_ROTATE_TIME = 1;//shape rotates every 1 second
 
     //soooo many flags.........lol
     //when we're ready to start flag
@@ -226,6 +242,15 @@ public class ExpController : MonoBehaviour
     protected float _trialTimer = 0;
     protected bool _trialLoading = true;
 
+    //slot marker timer
+    protected float _slotMarkerTimer = 0;
+
+     
+
+    //shape rotate timer
+    protected float _rotateTimer = 0;
+
+    //saving flags
     protected bool _savingEntry = false;
     protected bool _savedEntry = false;
 
@@ -286,6 +311,12 @@ public class ExpController : MonoBehaviour
                 SaveEntry(NO_RESPONSE);
                 NextTrial();
             }
+        }
+
+        if(spawn != null && expType == ExpType.RotatingComplex)
+        {
+            spawn.transform.parent.Rotate(m_ShapeRotAxis * m_ShapeRotSpeed * Time.deltaTime);
+            _rotateTimer = 0;
         }
     }
 
@@ -496,14 +527,14 @@ public class ExpController : MonoBehaviour
             m_SortingCube.LookAt(_playerTransform.position, _shapeToBoxRotation1[shape][0]);
             m_SortingCube.Rotate(_shapeToBoxRotation1[shape][1]);
 
-            m_ShapeSpawn[loc].transform.LookAt(_playerTransform.position, Vector3.up);
+            //m_ShapeSpawn[loc].transform.LookAt(_playerTransform.position, Vector3.up);
         }
         else
         {
             m_SortingCube.eulerAngles = _shapeToBoxRotation0[shape];
             //m_ShapeSpawn[loc].eulerAngles = new Vector3(90, 90, 0);
             //m_ShapeSpawn[loc].eulerAngles = new Vector3(90, -90, 0);
-            m_ShapeSpawn[loc].eulerAngles = new Vector3(90, -180, -180);
+            //m_ShapeSpawn[loc].eulerAngles = new Vector3(90, -180, -180);
         }
 
         //activate the shape slot marker
@@ -518,9 +549,13 @@ public class ExpController : MonoBehaviour
 
     public void SetExpType(int type)
     {
-        _expType = (ExpType)type;
+        expType = (ExpType)type;
+    }
 
-        if(type == 0 || type == 2)
+    protected void ExpTypeChanged()
+    {
+        //Non Interact Simple, Non Interact Complex
+        if (expType == ExpType.NonInteractiveSimple || expType == ExpType.NonInteractiveComplex)
         {
             m_FacePlayer = false;
             m_RandomShapeLocation = false;
@@ -528,12 +563,13 @@ public class ExpController : MonoBehaviour
             m_MinBlankTimeLimit = 3; m_MaxBlankTimeLimit = 300; m_TrialTimeLimit = 2.5f;
             m_NumberSmaller = DEFAULT_NO_SMALLER; m_NumberLarger = DEFAULT_NO_LARGER; m_NumOfShapes = DEFAULT_NO_SHAPE;
 
-            if (type == 0)
+            if (expType == ExpType.NonInteractiveSimple)
                 m_Shapes = m_RegularShapes;
-            if (type == 2)
+            if (expType == ExpType.NonInteractiveComplex)
                 m_Shapes = m_ComplexShapes;
         }
-        else if(type == 1 || type == 3)
+        //Interact Simple, Interact Complex
+        else if (expType == ExpType.InteractiveSimple || expType == ExpType.InteractiveComplex)
         {
             m_FacePlayer = false;
             m_RandomShapeLocation = false;
@@ -541,14 +577,25 @@ public class ExpController : MonoBehaviour
             m_MinBlankTimeLimit = 3; m_MaxBlankTimeLimit = 300; m_TrialTimeLimit = 10f;// 5f;
             m_NumberSmaller = DEFAULT_NO_SMALLER; m_NumberLarger = DEFAULT_NO_LARGER; m_NumOfShapes = DEFAULT_NO_SHAPE;
 
-            if (type == 1)
+            if (expType == ExpType.InteractiveSimple)
                 m_Shapes = m_RegularShapes;
-            if (type == 3)
+            if (expType == ExpType.InteractiveComplex)
                 m_Shapes = m_ComplexShapes;
+        }
+        //Rotating Complex
+        else if (expType == ExpType.RotatingComplex)
+        {
+            m_FacePlayer = false;
+            m_RandomShapeLocation = false;
+            m_CanGrabBox = false; m_CanGrabShapes = false;
+            m_MinBlankTimeLimit = 3; m_MaxBlankTimeLimit = 300; m_TrialTimeLimit = 10f;// 5f;
+            m_NumberSmaller = DEFAULT_NO_SMALLER; m_NumberLarger = DEFAULT_NO_LARGER; m_NumOfShapes = DEFAULT_NO_SHAPE;
+
+            m_Shapes = m_ComplexShapes;
         }
         else
         {
-            RLogger.Log($"ExpType {type} not valid");
+            RLogger.Log($"ExpType {expType.ToSafeString()} not valid");
         }
     }
 
@@ -696,7 +743,7 @@ public class ExpController : MonoBehaviour
     /// </summary>
     protected void CreateExpFile()
     {
-        m_DataManager.CreateExpFile($"Exp{_expType.ToSafeString()}_{m_PID}_{m_Condition}_{m_Repeats}Reps_{m_ScaleDiff}Range");
+        m_DataManager.CreateExpFile($"Exp{expType.ToSafeString()}_{m_PID}_{m_Condition}_{m_Repeats}Reps_{m_ScaleDiff}Range");
         /*FILE_PATH = $"{Application.persistentDataPath}/ExpEntry_{System.DateTime.Now:yyyy-MM-dd-HH_mm_ss}.csv";
         Debug.Log("fILE PATH IS: " + FILE_PATH);
 
@@ -824,7 +871,14 @@ public class ExpController : MonoBehaviour
             spawn.transform.localScale.y * 1,
             spawn.transform.localScale.z * size);
         if (m_FacePlayer)
+        {
+            spawn.transform.LookAt(_playerTransform.position, Vector3.up);
             spawn.transform.Rotate(new Vector3(90, 0, 0));
+        }
+        else
+        {
+            spawn.transform.eulerAngles = new Vector3(90, -180, -180);
+        }
         if (spawn.TryGetComponent<Rigidbody>(out Rigidbody r))
         {
             r.mass = 1e+09f;
